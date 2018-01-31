cd8241fa3f2df41e304d326690e2dd0e449fa29d
Orin Fletcher <orinf@yahoo.com>
2018-01-31 13:19:06 -0800

    completed first exercise

---
+++ JS Exercises/I_Do_Declare.js
@@ -0,0 +1,23 @@
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+
+var string1 = "Coding";
+var string2 = "Dojo";
+var string3 = "Rocks";
+
+var bool1 = true;
+var bool2 = false;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+
+console.log(bool1);
+console.log(bool2);
