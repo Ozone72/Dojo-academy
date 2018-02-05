34bfbaae0b5ab415f17b5b5e4b39c981334cda17
Orin Fletcher <orinf@yahoo.com>
2018-02-04 18:19:13 -0800

    numbers_only.js

---
+++ JS Exercises/numbers_only.js
@@ -0,0 +1,11 @@
+function numbersOnly(arr){
+    var newArr = [];
+    for(var i=0; i<arr.length; i++){
+        if(typeof(arr[i])==="number"){
+            newArr.push(arr[i]);
+        }
+     }
+     console.log(newArr);
+ }
+ numbersOnly([1, "apple", -3, "orange", 0.5]);
+ numbersOnly(["nope", "still no", true, 4,5,6]);
