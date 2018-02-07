2b084de5e20be21a77d28a906e1a4082af001c22
Orin Fletcher <orinf@yahoo.com>
2018-02-06 19:34:19 -0800

    stuff

---
+++ JS Exercises/students_instruct.js
@@ -0,0 +1,28 @@
+// var students = [ 
+//     {first_name:  'Tom', last_name : 'Brady'},
+//     {first_name : 'Danny', last_name : 'Amendola'},
+//     {first_name : 'Rob', last_name : 'Gronkowski'},
+//     {first_name : 'Deon', last_name : 'Lewis'},
+//     {first_name: 'James', last_name: 'White'}
+// ];
+// function instructors(arr){
+//     for(var i=0; i<arr.length; i++){
+//         console.log(students[i].first_name, students[i].last_name);
+//     }
+// }
+// instructors(students);
+
+var users = {
+    'Students': [ 
+        {first_name:  'Michael', last_name : 'Jordan'},
+        {first_name : 'John', last_name : 'Rosales'},
+        {first_name : 'Mark', last_name : 'Guillen'},
+        {first_name : 'KB', last_name : 'Tonel'}
+     ],
+    'Instructors': [
+        {first_name : 'Michael', last_name : 'Choi'},
+        {first_name : 'Martin', last_name : 'Puryear'}
+     ]
+    }
+
+    
