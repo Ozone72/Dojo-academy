7447e80bdbdfdfb7148ac2c9a3fa1e81d32a907f
Orin Fletcher <orinf@yahoo.com>
2018-02-08 16:16:11 -0800

    changed function names

--- Algorithms/Basic13.js
+++ Algorithms/Basic13.js
@@ -2,33 +2,27 @@
 
 // Print 1-255
 // Print all the integers from 1 to 255.
-// Inputs: n/a
-// Outputs: prints to console 1 to 255.
-function printFromTo(){
-    for (let i = 1; i <= 255; i++) {
-        console.log(i);
-    }
-}
-printFromTo();
+// function printFromTo(){
+//     for (let i = 1; i <= 255; i++) {
+//         console.log(i);
+//     }
+// }
+// printFromTo();
 
 // Print Sum 0-255
 // Print integers from 0 to 255, and with each integer print the sum so far.
-// Inputs: none
-// Outputs: prints to screen 0 to 255, and with each integer, also print the current sum
-// function basic2(){
+// function printSum(){
 //     let sum = 0;
 //     for (let i = 0; i <= 255; i++) {
 //         sum+=i;
 //         console.log(`Value of i: ${i} \nCurrent sum: ${sum}`);
 //     }
 // }
-// basic2();
+// printSum();
 
 // Find and Print Max
 // Given an array, find and print its largest element.
-// Inputs: an array 
-// Outputs: print to screen the largest element of the given array
-// function basic3(array){
+// function printMax(array){
 //     let max = array[0];
 //     for (let index = 0; index < array.length; index++) {
 //         if (array[index]>max) {
@@ -37,26 +31,22 @@ printFromTo();
 //     }
 //     console.log(`The largest element in the array is: ${max}`);
 // }
-// basic3([1,5,4,6,7,65,34,-23,0]);
+// printMax([1,5,4,6,7,65,34,-23,0]);
 
 // Array with Odds
 // Create an array with all the odd integers between 1 and 255 (inclusive).
-// Inputs: n/a
-// Outputs: returns an array with odd integers from 1 to 255 inclusive
-// function basic4(){
+// function arrayWithOdds(){
 //   let array = [];
-//   for (let i = 1; i <= 255; i++) {
+//   for (let i = 1; i <= 255; i+=2) {
 //     array.push(i);
 //   }
 //   return(array);
 // }
-// console.log(basic4());
+// console.log(arrayWithOdds());
 
 // Greater Than Y
 // Given an array and a value Y, count and print the number of array values greater than Y.
-// Inputs: an array of numbers, and a number Y to compare to
-// Outputs: a count of the elements in the array that are greater than Y
-// function basic5(array, Y){
+// function greaterThanY(array, Y){
 //   let count = 0;
 //   for (let i = 0; i < array.length; i++) {
 //     if(array[i]>Y){
@@ -66,13 +56,11 @@ printFromTo();
 //   // Template literals are awesome
 //   console.log(`Number of elements in the array greater than ${Y}: ${count}`);
 // }
-// basic5([78,2,6,4,5,6,1,23],2);
+// greaterThanY([-2,2,6,4,5,6,1,23],2);
 
 // Max, Min, Average
 // Given an array, print the max, min and average values for that array.
-// Inputs: an array of numbers
-// Outputs: Print the max, min, and average values of the array
-// function basic6(array) {
+// function maxMinAvg(array) {
 //   let max = array[0];
 //   let min = array[0];
 //   let sum = 0;
@@ -87,11 +75,11 @@ printFromTo();
 //   }
 //   console.log(`Max value in the array: ${max} \nMin value in the array: ${min} \nAverage for values in the array: ${sum /(array.length)}`);
 // }
-// basic6([1,4,6,75,23,100]);
+// maxMinAvg([1,4,6,75,23,100]);
 
 // Swap String For Array Negative Values
 // Replace any negative array values with 'Dojo'.
-// function basic7(array){
+// function swapString(array){
 //   for (let index = 0; index < array.length; index++) {
 //     if(array[index]<0){
 //       array[index]='Dojo';
@@ -99,16 +87,16 @@ printFromTo();
 //   }
 //   return array;
 // }
-// console.log(basic7([1,3,-76,4,3,-1]));
+// console.log(swapString([1,3,-76,4,3,-1]));
 
 // Print Odds 1-255
 // Print all odd integers from 1 to 255.
-// function basic8(){
+// function printOdds(){
 //   for (let index = 1; index < 255; index+=2) {
 //     console.log(index);
 //   }
 // }
-// basic8();
+// printOdds();
 
 // Iterate and Print Array
 // Iterate through a given array, printing each value.
@@ -129,7 +117,7 @@ printFromTo();
 //   avg = avg/(array.length);
 //   console.log(`The average of the elements in the array is: ${avg}`);
 // }
-// printAvg([4,7,12,15,4,4,2]);
+// printAvg([4,12,2,2,23]);
 
 // Square the Values
 // Square each value in a given array, returning that same array with changed values.
