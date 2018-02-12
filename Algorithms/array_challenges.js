31f0d4f2d9aabb902fe4cd4d9bfe1fb65360adaa
Orin Fletcher <orinf@yahoo.com>
2018-02-12 12:44:12 -0800

    pushFront complete

---
+++ Algorithms/array_challenges.js
@@ -0,0 +1,32 @@
+//Array Challenges homework for 2/14
+//Methods allowed are .push() and .pop() - otherwise, nope.
+//TEST ARRAYS:
+var arrayNum = [1,2,3,65,9,7];
+var arrayNeg = [-3,-4,-11,-20,-23];
+var arrayStrng = ["Coding", "Dojo", "Chewy", "nomnom"];
+var arrMixType = [
+    {dog: "Chewy",
+     cat: "Leo"}, 
+     [1,2,3],
+     true,
+     "Puppies rule.",
+     "Kitties drool."
+];
+var arrEdge = [undefined, Infinity, 4, 0, -1, "Weird", true];
+
+//PushFront
+// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods. PushFront(arr,val)
+function pushFront(arr, val){
+//     arr.length = arr.length+1;
+//     for(var i=arr.length-1; i>=1; i--){
+//         arr[i]=arr[i-1];
+//     }
+//     arr[0]=val;
+//     console.log(arr);
+//     return arr;
+// }
+// pushFront(arrayNum, 42);
+// pushFront(arrayNeg, 42);
+// pushFront(arrayStrng, 42);
+// pushFront(arrMixType, 42);
+
