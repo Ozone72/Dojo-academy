19271c2122c6198baa8c140b1f87281ad8e390d3
Orin Fletcher <orinf@yahoo.com>
2018-03-17 13:11:54 -0700

    finished exam

--- Algorithms/algos_iii_exam.js
+++ Algorithms/algos_iii_exam.js
@@ -30,7 +30,12 @@ function sList() {
     // With an sList object of { head: (1)->(2)-x }
     // Add a length property to the object with the right count { head: (1)->(2)-x, counter: 2 }
     current = this.head;
-    this.len = 1;
+    this.length = 0;
+    while (current) {
+      this.length++;
+      current = current.next;
+    }
+
     try {
     } catch (err) {
       console.log("Invalid Code", err);
@@ -41,7 +46,14 @@ function sList() {
     // Update the length property of the sList based on the number of nodes in the sList.
     // With an sList object of { head: (1)->(2)->(3)-x , counter: 0 }
     // Update the counter property: { head: (1)->(2)->(3)-x , counter: 3 }
+
     try {
+      this.length = 0;
+      current = this.head;
+      while (current) {
+        this.length++;
+        current = current.next;
+      }
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -52,6 +64,13 @@ function sList() {
     // With an sList of (1)->(2)->(3)-x and given val: 4
     // Alter the sList to be (1)->(2)->(3)->(4)-x
     try {
+      current = this.head;
+      newNode = new slNode(val);
+      //   console.log(newNode);
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -62,6 +81,12 @@ function sList() {
     // With a sList of (1)->(2)->(3)-x
     // Alter the sList to be (1)->(2)-x
     try {
+      current = this.head;
+      while (current.next.next) {
+        current = current.next;
+      }
+      current.next = null;
+      //   console.log("current: ", current);
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -72,6 +97,12 @@ function sList() {
     // With a sList of (1)->(2)->(3)->(4)->(5)-x and given num: 3
     // Alter the sList to be (1)->(2)-x
     try {
+      this.updateLength();
+      after = this.head;
+      for (var i = 1; i < this.length - num; i++) {
+        after = after.next;
+      }
+      after.next = null;
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -98,6 +129,13 @@ function dList() {
   this.pushNode = function(val) {
     // Add a node at the end of the dList with a value of (val)
     try {
+      //   console.log("val: ", val);
+      end = this.tail;
+      newNode = new dlNode(val);
+      end.next = newNode;
+      newNode.prev = end.next;
+      this.tail = newNode;
+      //   console.log(this);
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -106,6 +144,11 @@ function dList() {
   this.popNode = function() {
     // Remove the last node from the dList.
     try {
+      head = this.head;
+      tail = this.tail;
+      tail = tail.prev;
+      tail.next = null;
+      //   console.log("tail: ", tail);
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -116,6 +159,21 @@ function dList() {
     // With a dList of (1)<=>(2)<=>(3)<=>(4), val of 9 and mark of 3
     // Alter the dList to (1)<=>(2)<=>(9)<=>(3)<=>(4)
     try {
+      //   console.log("this.head: ", this.head);
+      //   console.log("this.tail: ", this.tail);
+      before = this.head;
+      after = this.head;
+      newNode = new dlNode(val);
+      //   console.log("newNode: ", newNode);
+      while (after.val !== mark) {
+        after = after.next;
+        before = before.next;
+      }
+      before = before.prev;
+      newNode.next = after;
+      newNode.prev = before;
+      before.next = newNode;
+      after.prev = newNode;
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -126,6 +184,18 @@ function dList() {
     // With a dList of (1)<=>(2)<=>(2)<=>(1) return true
     // With a dList of (1)<=>(2)<=>(3)<=>(4)<=>(1) return false
     try {
+      start = this.head;
+      //   console.log("start: ", start);
+      end = this.tail;
+      //   console.log("end: ", end);
+      while (start !== end) {
+        if (start.val !== end.val) {
+          return false;
+        }
+        start = start.next;
+        end = end.prev;
+      }
+      return true;
     } catch (err) {
       console.log("Invalid Code", err);
     }
@@ -203,10 +273,9 @@ score = +String.fromCharCode(48);
     if (
       slist.head &&
       slist.head.next &&
-      slist.head.next &&
       slist.head.next.next &&
       slist.head.next.next.next &&
-      slist.head.next.next.next.val === 5
+      slist.head.next.next.next.next.val === 5
     ) {
       console.log("sList - pushNode - PASS ");
       score = score + +String.fromCharCode(49);
